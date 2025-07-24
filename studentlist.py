20c3dbe0b225d30d74b5c53cf1f401597b3b0a91
ellastar-b <stellamoses081@gmail.com>
2025-07-24 17:34:41 +0100

    UPDATE

---
+++ studentlist.py
@@ -0,0 +1,361 @@
+preNurseryI = [
+               "Agu Chinedum Diewdonne", 
+               "Haggai Henry Haruna", 
+               "Josiah Lushan Star", 
+               "Okon Deborah", 
+               "Okoye Brandon Ifeanyichukwu", 
+               "Okaka Charles Chinemerie", 
+               "Ogunrombi Eliora", 
+               "Terseer Mesuur Beatrice", 
+               "Okunlola Elijah Morireoluwa"
+               ]
+preNurseryII = [
+  "Ameh Telvin",
+  "Itodo Prevail Ojumeyi ",
+  "Echekwu Kristine Chiaodi ",
+  "Assi Vitallis Abba",
+  "Theophilus Ekerini Ekong",
+  "Amaizu Bryan Chiemela",
+  "Nze Chidiebere Derick",
+  "Awa Adriel Obasioma",
+  "Dalama David Oluwafemi",
+  "Kayode Tiseoluwa ",
+  "Mgbemena  Jidenna Kanu",
+  "Oliver Valentine Chimnadindu",
+  "Osaywamen Ethan Ehosa",
+  "Ismail Habiba", 
+  "Idris Mohammed Zainab", 
+  "Sanusi Mohammad Moshood"
+]
+nurseryOneGold = [
+  "Abayomi Abdulrasaq Abayomi",
+  "Atom Ivan",
+  "Ajibola  Khalid Oche",
+  "Anyadike Chukwuebuka Elliot",
+  "Bukar Jamila",
+  "David Joan",
+  "Emmanuel Peace Eleojo",
+  "Emmanuel Joy Enyojo",
+  "Isaac Henrybright Ojochegbe",
+  "Mustapha Ahmad Lawal",
+  "Nnameka Kambili Chiam",
+  "Okafor Amazing",
+  "Owulo Olieje Henrick",
+  "Oladipopu Anita Adeola",
+  "Promise Favour Ojochegbe",
+  "Igoche Joan",
+  "Anthony E. Kamsiyochukwu"
+]
+nurseryOneDiamond = [
+  "Abdulrasaq Afsheen Enehezeyi",
+  "Goodfriday Ayibatari-Emitimipah",
+   "Aiya Derick Ehizokhar",
+   "Solomon James Magodeshekwoyi"
+    "Ameh Ejebabi John",
+    "Muhammed Ahmed Lukman",
+    "Lucky Mishael Ovie",
+    "Ubah Michael",
+    "Peniel Enang"
+  
+]
+nurseryTwo = [
+  "Amaizu Chisom Barbara",
+  "Abdulwaheed Haadiya Onize",
+  "Ayodele Pharez Oluwapelumi",
+  "Adam Abdulmumin Onimisi",
+  "Echekwu Fina Chinyere",
+  "Eghosa Umammenosa Nathaniel",
+  "Emmanuel Kingjesse Oche",
+  "Emezuom Wisdom Obichukwu",
+  "Johnson Jaqzah Ojoanya",
+  "Kayode Tiwaloluwa Dumebi",
+  "Mark Prince-excel",
+  "Mohammed Yasir Zakari",
+  "Nze Nora Oluchi",
+  "Okon Jayden",
+  "Uzuegbu Chisom Stanley",
+  "Yakubu Mateenah Oko’osu",
+  "William Eno Elianagreat",
+  "Magnificent Godspower", 
+  "Agbo Samuel Chinonso", 
+  "Friday Dominion Kosi", 
+  "Udogo Stella Oluhi"
+]
+
+gradeOneDiamond = [
+    "Adams Utenojo Kadosh",
+    "Akinade Anuoluwapo Dominion",
+    "Christopher Daniel",
+    "Enang Praise",
+    "GoodFriday WoyinTari-Ere Timipah",
+    "Haruna Salem Goma",
+    "KingJames Wendy Barisua",
+    "Kanu-Mgbemena Tobechukwu",
+    "Muhammed Bilkisu",
+    "Nosakhare Henry Osarodion",
+    "Nwokoshiyole Munachi Light of God",
+    "Oloniyo Queen-Esther",
+    "ANTHONY CHIMAMADA KENDRA"
+  ]
+gradeOneGold=[
+    "Abraham Odinaka Bethel",
+    "Animasahun Abiodun Samirah",
+    "Ayibaebi Wisdom",
+    "Dimka Riyeh",
+    "Ejeh Ihortu Jedidiah",
+    "Emubarsa Jared Matthew",
+    "Francis Clare Chimamanda",
+    "Ikechiuku Treasure Munachi",
+    "Johnson Ochanya Jaana",
+    "Oliver Valentine Chimamanda",
+    "Raji Irewa Gift",
+    "Stanley Sasodje Annabel",
+    "Ubah Michelle"
+  ]
+
+grade2 =[
+  "Abel Isabel", 
+  "Atom .Z. Adel", 
+  "Ayodele Adrian",
+  "Ayodele Peniel", 
+  "Dickman Hyehirra Crown", 
+  "Joseph Meekness", 
+  "Lar Chloe", 
+  "Mohammed Abdulhameed", 
+  "Ndackson Joel", 
+  "Nze Ifeoma Mirabel", 
+  "Oladipupo Ann", 
+  "Onyekachi Precious", 
+  "Onyedikachi Mmesoma", 
+  "Owulo Harris", 
+  "Stephen Somtochukwu", 
+  "Friday Kamsiyochukwu Wisdom"
+  ]
+
+gradeThree= [
+    "AbdulWaheed Muhsin",
+    "Agbonlahor Joy Owen",
+    "Akobe Itopa Sharon",
+    "Aliu Cherish",
+    "Ameh Benedict Ojongungwa",
+    "Awa Prudence Nkemruturym",
+    "Ayibaebi Brighen",
+    "Igbokwe Chibusoma Favour",
+    "Lucky Samuel Ejiro",
+    "Oigene  Steed Edoh ",
+    "Seth Tamara Sambo",
+    "Ubah Dominic Kachi",
+    "EZEANYA BELUSOCHUKWU FRANKLYN JNR"
+  ]
+
+gradeFourGold = [
+  "Abraham Bryan Osinachi",
+  "Attah Adam Attah",
+  "Akpati David Ifeakachukwu",
+  "Archibong Gabriella",
+  "Assi Alexis Stephen",
+  "Daniel Delight Pipkong",
+  "Francis Blaise Munachimuso",
+  "Lar Nankpah Jesse",
+  "Ngene Franklin Ebubechukwu",
+  "Raji Oluwapamilerin Gold",
+  "Stephen Immaculate Osinachi",
+  "Yakubu Ibrahim Udaba",
+  "Ige Deborah Mercy"
+]
+
+gradeFourDiamond = [
+  "Abel Loretta Chubiyojo",
+  "Adibe Chimeuba David",
+  "Ameh Ofowecho Nathaniel",
+  "Ayodele Damilare Louis",
+  "Echekwu Ucheh Hilda",
+  "Ewe Ulyamachuwe Joanna",
+  "Eze Amarachi Anastasia",
+  "Goodfriday Tarimobowei",
+  "Haruna Dasa Stephanie",
+  "Ibrahim Olorundare Ismail",
+  "Jonathan Kappe Jesslyn",
+  "Olorunleke Ogoluwa Melody",
+  "Solomon Shekwodeza Jeffery",
+  "Uba Chizaram Collins",
+  "Uka Onyedikachukwu Emmanuel"
+]
+
+gradeFive = [
+  "Adegoke Joshua",
+  "Aliu Charis",
+  "Ayodele Dunmininu",
+  "Azifuaku Prevall Chidozie",
+  "Chukwudi Greta Akudo",
+  "Onuh Sarah",
+  "Sasodje Zoe",
+  "Seth Terry",
+  "Zira Divine",
+  "Abdulmajid Nabila",
+  "Abel Mirabel Ojochegbe",
+  "Akobe Itopa Susan",
+  "Bamidele Hephzibah Antola",
+  "David Daniella Anibe",
+  "Fannap Keren Nanre",
+  "Bassey Olawole Godwin",
+  "Okaka Kingsley Chigozim",
+  "Sanusi M. Rodiat Folashade",
+  "Nelson Owen-Joshua O."
+]
+
+
+
+
+
+
+
+js2G = [
+  "Agunbiade Peace",
+  "Alhamdu Grace",
+  "Ameh Victory",
+  "Adegboyega Angel",
+  "Daniel David",
+  "Enang Prosper",
+  "Emmanuel Betting",
+  "Gabriel Axel",
+  "Kerry Taiwo Wonder",
+  "Mmaduka Success",
+  "Obasi Angel Ndubisi",
+  "Olurotimi Joseph",
+  "Sanusi Raheemat",
+  "Sadaojie  Alexander",
+  "Too Chidume Rex Leo"
+]
+
+js2D = [
+  "Ewe Vanessa",
+  "Onuh Samuel",
+  "Eze Casmir",
+  "Adegoke Samuel",
+  "Oche Isabella",
+  "Davdonald Arella",
+  "Michael Sharon",
+  "Olurotimi Joshua",
+  "Akwaranadu Emmanuel",
+  "Jude Joash",
+  "Bamidele Elizabeth",
+  "Ochi Destiney",
+  "Bassey Miracle",
+  "Jacob Gracious"
+]
+
+
+js3D = [
+  "Abraham Blossom",
+  "Adegboyega Judah",
+  "Anyiam Amarachi",
+  "Ayodele Vincent",
+  "Desmond Princess",
+  "Ekezie Jeremiah",
+  "Emmanuel Olan",
+  "Francis Charles",
+  "Ihiale Freedom Frank ",
+  "Haruna  Success",
+  "Kappe Jonathan Jesse",
+  "Monday Fortune",
+  "Otu Esther",
+  "Simeon Tracy",
+  "Solomon Jethro",
+  "Wueseter Barnabas",
+  "Lucky Joanna",
+  "Yakubu Eram"
+]
+
+js3G = [
+  "Anaela Miracle",
+  "Ayinde David",
+  "Chukwudi Leora",
+  "Ekpe Vine Erela",
+  "Friday Angela",
+  "Jacob Maxwell",
+  "Jeremiah Oshoma",
+  "Lucky Emmanuella",
+  "Matthew Yamura Joy",
+  "Ndackson Joshua",
+  "Ngene David ",
+  "Obe Prince ",
+  "Oigene Daniella",
+  "Oshinaya Craig",
+  "Okpanah Emmanuel",
+  "Solomon Joan",
+  "Tanko Hadiza",
+  "Uka Amarachi"
+]
+
+js1G = [
+  "Awa Wealth Chiamanda",
+  "Assi Joshua Anderson",
+  "Adibe Faith Chinesom",
+  "Archibong Splendid A",
+  "Hindan Vivian Mimidoo",
+  "Michael Praise Oluwadamilola",
+  "Malomo Jasmine F.",
+  "Mathew Tehillah K.",
+  "Obe Princess Miracle",
+  "Oigene Dominion Daniels",
+  "Owulo Onah Henry",
+  "Thomas Great Courage",
+  "Echeku Daniella"
+]
+
+
+js1D = [
+  "Adams Ayodeji ",
+  "Adams Emmanuel",
+  "Alfred Bethel Oluwafikayomi",
+  "Anaele Marvelous",
+  "Anaele Micheal",
+  "Owulo Onah",
+  "Noah Naomi",
+  "Matthas Gianne ",
+  "Sylvester C. Havilah",
+  "Uchenna Stacie",
+  "Amakoromo Faith"
+]
+
+SS1science = [
+  "Adams Flourish",
+  "Akindigh Keziah",
+  "Assi Alison",
+  "Chukwudi Lucian",
+  "Ejigah ThankGod",
+  "Innocent Cecilia",
+  "Kerry-Taiwo Treasure",
+  "Kingjames Bianca",
+  "Noah Joseph",
+  "Olorunleke Michael"
+]
+
+ss1art = [
+  "Odum Harmony"
+]
+
+ss2science = [
+  "Adebayo A. Princess",
+  "Emmanuel J. Marcus",
+  "Lanloye E. Ololade",
+  "Lawrence O. Angel",
+  "Mathew E. Grace",
+  "Olurotimi O. Emmanuel",
+  "Onyealum F. Chioma"
+]
+
+ss2art = [
+    "Eze E. Esther",
+  "Gopala Victor"
+]
+
+ss3science =[
+  "Onyebara Favour Dumaka",
+  "Yakubu Mohammad"
+]
+
+ss3art = [
+  "Monday Praise Oche",
+]
